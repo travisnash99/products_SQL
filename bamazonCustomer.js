f1f40aac17110b6681dcc58cb20ae056eddb5336
Travis Nash <travis.nash99@gmail.com>
2017-04-15 15:53:29 -0500

    JS updates

---
+++ bamazonCustomer.js
@@ -0,0 +1,39 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+//    Your username
+  user: "root",
+
+//    Your password
+  password: "",
+  database: "bamazonDB"
+});
+connection.connect(function(err) {
+  if (err) throw err;
+});
+connection.query("SELECT * FROM products", function(err, res) {
+  for (var i = 0; i < res.length; i++) {
+    console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+  }
+  console.log("-----------------------------------");
+});
+
+var start = function() {
+  inquirer.prompt({
+    name: "user",
+    type: "list",
+    message: "Please enter the product number.",
+    choices: ["1", "2", "3", "4","5", "6", "7"]
+//   }).then(function(answer) {
+//      if (answer.user() === "1", "2", "3", "4","5", "6", "7") {
+//        console.log;
+//      }
+//      else {
+//        bidAuction();
+//      }
+   })
+};
